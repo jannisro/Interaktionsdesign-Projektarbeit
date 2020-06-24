7a3a3965de88089c83ec63e13bf7674a705a8f10
Jannis Rohden <kontakt@jannisrohden.de>
2020-06-24 12:55:08 +0200

    Profil-Transition begonnen

--- assets/master.js
+++ assets/master.js
@@ -2,3 +2,47 @@ $('.accordion__header').on('click', function () {
     $(this).siblings('.accordion__body').toggle();
     $(this).parent().toggleClass('open');
 });
+
+function loadProfile(name) {
+    const content = JSON.parse($('#' + name).html());
+    $('#popup-name').html(content.name);
+    $('#popup-location').html(content.location);
+    $('#popup-age').html(content.age);
+    $('#popup-image').css('background-image', 'url(' + content.image + ')');
+    $('#popup-text').html(content.text);
+    let sportsList = '';
+    content.sports.forEach(function (item) {
+        let time = ambition = '';
+        if (item.time) {
+            time = '<p>Lieblingszeit: '+item.time+'</p>';
+        }
+        if (item.ambition && item.ambition.length > 0) {
+            ambition = '<p>Ziele: ';
+            ambition += item.ambition.join(', ');
+            ambition += '</p>';
+        }
+        sportsList += '<li class="list-group-item"><p><b>' + item.name + '</b> (' + item.skill + ')</p>' + time + ambition + '</li>';
+    });
+    $('#popup-sports').html(sportsList);
+}
+
+function setPopupSize() {
+    $('.popup').css('width', $('.container').innerWidth() + 'px');
+    $('.popup').css('height', $(window).innerHeight() - $('.menu').innerHeight());
+    $('.popup').css('margin-left', ($(window).innerWidth() - $('.container').innerWidth()) / 2 + 'px');
+    if ($(window).innerWidth() >= 800) {
+        $('.popup').css('margin-top', $('.menu').innerHeight());
+    }
+}
+
+$('.feed-item').on('click', function () {
+    setPopupSize();
+    loadProfile($(this).attr('data-src'));
+    $('.popup').fadeIn(200);
+    $('.blur').fadeIn(200);
+});
+
+$('.popup__close, .blur').on('click', function () {
+    $('.popup').fadeOut(200);
+    $('.blur').fadeOut(200);
+});
